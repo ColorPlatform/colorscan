b8543eb95d43800c7f136feccb3e44ecedcf21d8
Faisal Naveed <faisal@rnssol.com>
2019-12-02 16:38:23 +0500

    updating start-dockrer.sh

--- start-docker.sh
+++ start-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Starting Docker"
-docker run -d -p 3000:3000  -e MONGO_URL=mongodb://rnssol:rnssol1234@18.223.52.186:27017/colorplatform -e METEOR_SETTINGS="$(cat settings.json)" --name explorer rnssolutions/explorer:0.1.3
+docker run -d -p 3000:3000  -e MONGO_URL=mongodb://rnssol:rnssol1234@18.223.52.186:27017/colorplatform -e METEOR_SETTINGS="$(cat settings.json)" --name explorer rnssolutions/explorer:0.1.4
 
