ba363eeac734a4041a24810f8ecfd0c8d771a995
Nikolay Pakulin <nikolay@paxdatatech.com>
2020-01-23 18:10:56 +0900

    Updated for mainbeta-2

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "Builing for production..."
-rm -rf bundle
 meteor build output/ --architecture os.linux.x86_64 --server-only
+
+rm -rf bundle
 tar -xvf output/explorer.tar.gz
 rm -rf output
 
--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "stating for production..."
-echo $MONGO_URL
-echo $METEOR_SETTINGS
-PORT=3000 ROOT_URL=http://localhost/ MONGO_URL=$MONGO_URL METEOR_SETTINGS=$METEOR_SETTINGS meteor node main.js
-
+# echo $MONGO_URL
+# echo $METEOR_SETTINGS
+# PORT=3000 ROOT_URL=http://localhost/ MONGO_URL=$MONGO_URL METEOR_SETTINGS=$METEOR_SETTINGS meteor node main.js
+PORT=3000 ROOT_URL=http://localhost/ MONGO_URL='mongodb://paxtest3:27017/colorplatform' METEOR_SETTINGS="$(cat settings.json)" meteor node ./server/main.js
 
