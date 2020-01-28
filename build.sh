1862a5274f05ea6a76825b3f088b375d993365d8
Nikolay Pakulin <nikolay@paxdatatech.com>
2020-01-28 19:05:01 +0900

    Keep builded bundle

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ meteor build output/ --architecture os.linux.x86_64 --server-only
 
 rm -rf bundle
 tar -xvf output/explorer.tar.gz
-rm -rf output
+# rm -rf output
 
 cd bundle/programs/server && meteor npm install --save
