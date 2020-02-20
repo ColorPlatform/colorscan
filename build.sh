7630fa30cb9e2b67adb62ea8b10c25cb03cd6d01
Nikolay Pakulin <nikolay@paxdatatech.com>
2020-02-21 01:11:29 +0900

    Build outside of the source tree

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Builing for production..."
-meteor build output/ --architecture os.linux.x86_64 --server-only
+meteor build ../output/ --architecture os.linux.x86_64 --server-only
 
 rm -rf bundle
-tar -xvf output/explorer.tar.gz
+tar -xvf ../output/explorer.tar.gz
 # rm -rf output
 
 cd bundle/programs/server && meteor npm install --save
