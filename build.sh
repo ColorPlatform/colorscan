62df091b351e36d5f12eff70a4592203f9943259
Nikolay Pakulin <nikolay@paxdatatech.com>
2020-02-21 07:20:48 +0900

    Modified for mainbeta-3

--- build.sh
+++ build.sh
@@ -3,8 +3,11 @@
 echo "Builing for production..."
 meteor build ../output/ --architecture os.linux.x86_64 --server-only
 
+echo BUILD FINISHED
+echo UNPACK BUNDLE
 rm -rf bundle
-tar -xvf ../output/explorer.tar.gz
+tar -xf ../output/explorer.tar.gz
 # rm -rf output
 
+echo INSTALL DEPENDENCIES
 cd bundle/programs/server && meteor npm install --save
